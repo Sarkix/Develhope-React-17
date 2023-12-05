1ae7102fc36f4e32ce34b91ac756e45662f941f7
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-12-05 21:02:13 +0100

    Añadir archivos ejercicio 17 React.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import InteractiveWelcome from "./InteractiveWelcome";
+
+function App() {
+  return (
+    <div>
+      <InteractiveWelcome />
+    </div>
+  );
+}
+
+export default App;
